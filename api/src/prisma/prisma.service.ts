3c2986a4b70d85db4d59a7238f8d76d384870f81
Shijin <138788615+Shijin2k3@users.noreply.github.com>
2026-07-04 11:52:19 +0530

    prisma nest js setup (#1)
    * prisma nest js setup
    
    * created buid file
    
    ---------
    
    Co-authored-by: shijin <shijinkumar@itoi.org>

---
+++ api/src/prisma/prisma.service.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+
+    super({
+      adapter,
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connection established.');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('Database connection closed.');
+  }
+}
