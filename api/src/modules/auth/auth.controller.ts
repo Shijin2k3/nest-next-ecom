7eb21e97fec1ee6e737045e2a01be25fcf5accf3
Shijin-ITOI <shijinkumar@itoi.org>
2026-07-24 10:03:31 +0530

    Sh/pipes (#3)
    * added pipe
    
    * added authentitcation like register ,login and logout
    
    * Update auth.controller.ts
    
    ---------
    
    Co-authored-by: Shijin <138788615+Shijin2k3@users.noreply.github.com>

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import { AuthResponseDto, LoginDto, RegisterDto } from './dto';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
 import { RefreshTokenGuard } from './guards/refresh-token.guard';
 import { JwtAuthGuard } from 'src/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -18,12 +19,48 @@ export class AuthController {
 
   @Post('register')
   @HttpCode(201)
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiResponse({
+    status: 201,
+    description: 'User Successfully Registered',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid Data',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'User Already Exists',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
     return this.authService.register(registerDto);
   }
 
   @Post('refresh')
   @UseGuards(RefreshTokenGuard)
+  @ApiBearerAuth('JWT-refresh')
+  @ApiOperation({
+    summary: 'Refresh Access Token',
+    description: 'Generates new access token using refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Tokens Successfully Refreshed',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Invalid Refresh Token',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
   async refresh(@GetUser('userId') userId: string) {
     return await this.authService.refreshTokens(userId);
   }
@@ -31,12 +68,27 @@ export class AuthController {
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-auth')
+  @ApiOperation({
+    summary: 'Logout User',
+    description: 'Log out user by revoking refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User Successfully Logged Out',
+  })
   async logout(@GetUser('userId') userId: string) {
     return this.authService.logout(userId);
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Login User' })
+  @ApiResponse({
+    status: 200,
+    description: 'User Successfully Logged In',
+    type: AuthResponseDto,
+  })
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
--- api/src/modules/auth/dto/register.dto.ts
+++ api/src/modules/auth/dto/register.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty()
   @IsNotEmpty({ message: 'Email Id is required' })
   @IsString()
   emailId!: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'Password is required' })
   @IsString()
   password!: string;
 
+  @ApiProperty()
   @IsNotEmpty({ message: 'First Name is required' })
   @IsString()
   firstName!: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   lastName?: string;
